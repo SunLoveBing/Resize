9a1f146816b6888c27b4da1f18752d356b41c81e
SunLoveBing <457285506@qq.com>
2016-01-24 16:00:09 +0800

    Picture Resize Code
    This code is used for picture resize. First, a blank picture is needed.
    Then, all the pictures will be resized to the same size as the blank
    picture.

---
+++ Demo.m
@@ -0,0 +1,9 @@
+clear all;
+close all;
+clc;
+
+BlankPicPath = 'D:\blank\';
+SrcImgPath = 'D:\Ô­Í¼Æ¬\';
+destination = 'D:\½á¹û\';
+
+PicResize(BlankPicPath,SrcImgPath,destination);
---
+++ PicResize.m
@@ -0,0 +1,40 @@
+function PicResize(BlankPicPath,SrcImgPath,destination)
+
+BlankPicPath = strcat(BlankPicPath,'blank.jpg');
+ImgPath = dir([SrcImgPath,'*','.jpg']);
+ImgNum = 0;
+for i = 1 : length(ImgPath)
+    ImgNum = ImgNum + 1;
+end
+ImgPathName = cell(ImgNum,1);
+destinationName = cell(ImgNum,1);
+for i = 1 : ImgNum
+    ImgPathName{i} = strcat(SrcImgPath,ImgPath(i).name);
+    destinationName{i} = strcat(destination,ImgPath(i).name);
+end
+
+for k = 1 : ImgNum
+    BlankPic = imread(BlankPicPath);
+    BlankPic_width = size(BlankPic,2);
+    BlankPic_height = size(BlankPic,1);
+    
+    Img = imread(ImgPathName{k});
+    Img_width = size(Img,2);
+    Img_height = size(Img,1);
+
+    Img = imresize(Img,[Img_height*BlankPic_width/Img_width,BlankPic_width]);
+    Img_height = size(Img,1);
+    diff = BlankPic_height - Img_height;
+
+    for i = BlankPic_height : -1 : 1
+        if i > diff
+            for j = 1 : BlankPic_width
+                BlankPic(i,j,1) = Img(i-diff,j,1);
+                BlankPic(i,j,2) = Img(i-diff,j,2);
+                BlankPic(i,j,3) = Img(i-diff,j,3);
+            end
+        end
+    end
+
+    imwrite(BlankPic,destinationName{k},'jpg');
+end
